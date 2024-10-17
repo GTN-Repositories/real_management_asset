7f1598500385564b60274d898b640fffd78a9a02
Aizenss <irsyaderpl1@gmail.com>
2024-10-17 22:03:52 +0700

    tamplate

---
+++ resources/views/admin/layout/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+    id="layout-navbar">
+    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+        <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+            <i class="ti ti-menu-2 ti-sm"></i>
+        </a>
+    </div>
+
+    <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+        <div class="navbar-nav align-items-center">
+            <div class="nav-item navbar-search-wrapper mb-0">
+                <div class="flex-grow-1">
+                    <span class="fw-medium d-block">John Doe</span>
+                </div>
+            </div>
+        </div>
+        <div class="navbar-nav align-items-center ms-auto">
+            <div class="nav-item navbar-search-wrapper mb-0">
+                <div class="flex-grow-1">
+                    <small class="text-muted">Admin</small>
+                </div>
+            </div>
+        </div>
+        <div class="navbar-nav flex-row align-items-center ms-auto  ">
+            <a href="auth-login-cover.html" target="_blank">
+                <i class="ti ti-logout me-2 ti-sm"></i>
+                <span class="align-middle">Log Out</span>
+            </a>
+        </div>
+    </div>
+
+    <!-- Search Small Screens -->
+    <div class="navbar-search-wrapper search-input-wrapper d-none">
+        <input type="text" class="form-control search-input container-xxl border-0" placeholder="Search..."
+            aria-label="Search..." />
+        <i class="ti ti-x ti-sm search-toggler cursor-pointer"></i>
+    </div>
+</nav>
