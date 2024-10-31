a9302ed5c57457af5763d371b7866c536cfe230a
Dekasha <121219290+Dekasha@users.noreply.github.com>
2024-10-31 16:34:56 +0700

    project view and flow login driver

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        if (Auth::user()->roles[0]->name === 'superAdmin') {
+            return redirect()->intended(route('dashboard', absolute: false));
+        }
+        return redirect()->intended(route('driver.index', absolute: false));
     }
 
     /**
--- resources/views/main/driver/index.blade.php
+++ resources/views/main/driver/index.blade.php
@@ -6,7 +6,7 @@
     <!-- Pricing Plans -->
     <section class="section-py first-section-pt">
         <div class="container">
-            <h2 class="text-center my-4">Pricing Plans</h2>
+            <h2 class="text-center my-4">Management Project</h2>
 
             <div class="row mx-0 gy-3 px-lg-5" id="manager-container">
                 <!-- Cards will be populated here by AJAX -->
@@ -32,21 +32,23 @@
                     <div class="col-md-4 mb-md-0 mb-4">
                         <div class="card border rounded shadow-none" style="height: 30rem;">
                             <div class="card-body">
-                                <div class="my-3 pt-2 text-center"></div>
+                                <div class="my-3 pt-2 text-center">
+                                <i class="ti ti-building text-primary display-3"></i>
+                                </div>
                                 <h3 class="card-title text-center text-capitalize mb-1">${manager.name}</h3>
                                 <ul class="ps-0 my-4 pt-2 circle-bullets">
                                     ${manager.assets.slice(0, 5).map(
                                         asset => (
                                             `<li class="mb-2 d-flex align-items-center">
-                                                        <i class="ti ti-point ti-lg"></i>
-                                                        ${asset.name}
-                                                    </li>`
+                                                            <i class="ti ti-point ti-lg"></i>
+                                                            ${asset.name}
+                                                        </li>`
                                         )
                                     ).join('')}
                                 </ul>
                             </div>
                             <div class="card-footer">
-                                <button class="btn btn-label-success d-grid w-100 select-project-btn" data-manager-name="${manager.name}">Pilih Management Project</button>
+                                <button class="btn btn-label-primary d-grid w-100 select-project-btn" data-manager-name="${manager.name}">Pilih Management Project</button>
                             </div>
                         </div>
                     </div>
