d43b686b7a0d0d43e9c5fcd18c6ae9463b975ad3
Dekasha <121219290+Dekasha@users.noreply.github.com>
2024-12-05 15:52:21 +0700

    Update 2024_12_05_084546_update_column_loadsheet.php

--- database/migrations/2024_12_05_084546_update_column_loadsheet.php
+++ database/migrations/2024_12_05_084546_update_column_loadsheet.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('loadsheets', function (Blueprint $table) {
-            $table->string('bpit')->change();
+            $table->string('bpit')->nullable()->change();
         });
     }
 
